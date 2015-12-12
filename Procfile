5d75de533c6b79fbcd162572d4a198208ba5a88d
Jonathan Como <jonathan.como@gmail.com>
2015-12-12 15:03:38 -0800

    Clear out python files

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn -b :$PORT -k gevent --log-file - shelf.app:app
--- tests/test_case.py
+++
@@ -1,8 +0,0 @@
-from flask_testing import TestCase
-
-from shelf.app import app
-
-
-class APITestCase(TestCase):
-    def create_app(self):
-        return app
